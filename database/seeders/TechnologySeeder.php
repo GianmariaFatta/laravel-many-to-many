24e315f392417a59a6d6a3fb517513c42304bd76
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-03-13 17:05:05 +0100

    add technology seedere

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,23 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            ['label' => 'HTML', 'color' => 'danger'],
+            ['label' => 'CSS', 'color' => 'info'],
+            ['label' => 'Javascript', 'color' => 'warning'],
+            ['label' => 'Bootstrap', 'color' => 'dark'],
+            ['label' => 'Vue', 'color' => 'success'],
+            ['label' => 'SASS', 'color' => 'danger-emphasis'],
+            ['label' => 'SQL', 'color' => 'primary'],
+            ['label' => 'PHP', 'color' => 'secondary'],
+        ];
+
+        foreach($technologies as $technology){
+            $new_technology= new Technology();
+            $new_technology->label= $technology['label'];
+            $new_technology->color= $technology['color'];
+
+            $new_technology->save();
+        }
     }
 }
