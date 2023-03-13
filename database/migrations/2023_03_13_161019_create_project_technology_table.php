6bf03747cd1eb4bdaffaf1d1f9ccbcf64249e791
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-03-13 17:21:18 +0100

    add table_bridge

---
+++ database/migrations/2023_03_13_161019_create_project_technology_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_technology', function (Blueprint $table) {
+            $table->id();
+            
+            // $table->unsignedBigInteger('project_id');
+            // $table->unsignedBigInteger('technology_id');
+
+            // $table->foreign('project_id')->references('id')->on('projects');
+            // $table->foreign('technology_id')->references('id')->on('technologies');
+
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->foreignId('technology_id')->constrained()->onDelete('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_technology');
+    }
+};
